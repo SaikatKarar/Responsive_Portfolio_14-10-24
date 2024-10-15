2ae69d6c68c3fe8af37069bac526982cd03c1162
SaikatKarar <kararsaikat@gmail.com>
2024-10-15 13:20:33 +0530

    Add Services Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './component/Navbar'
 import Hero from './component/Hero'
 import About from './component/About'
 import Projects from './component/Projects'
+import Services from './component/Services'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Services />
     </>
   )
 }
