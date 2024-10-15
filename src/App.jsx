8b6adc31d27ee82ae18183355263a1ff1ef02591
SaikatKarar <kararsaikat@gmail.com>
2024-10-15 12:40:03 +0530

    Add Projects Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './component/Navbar'
 import Hero from './component/Hero'
 import About from './component/About'
+import Projects from './component/Projects'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
     </>
   )
 }
